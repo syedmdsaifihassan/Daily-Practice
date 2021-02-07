471e285b64ffb777ed1717ded03182b520e449f9
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-02-08 01:15:33 +0530

    Update Day_2.java

--- Hacker Rank (30 Days of Code)/Day_2.java
+++ Hacker Rank (30 Days of Code)/Day_2.java
@@ -2,7 +2,7 @@
 
 /*
 Objective
-In this challenge, you will work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video.
+In this challenge, you will work with arithmetic operators.
 
 Task
 Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
