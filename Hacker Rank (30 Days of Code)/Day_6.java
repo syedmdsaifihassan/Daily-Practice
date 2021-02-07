a1d322ede99398ceb72460c6c0a3c7361a276d61
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-02-08 01:13:34 +0530

    Update Day_6.java

--- Hacker Rank (30 Days of Code)/Day_6.java
+++ Hacker Rank (30 Days of Code)/Day_6.java
@@ -1,6 +1,6 @@
 /*
 Objective
-Today we will expand our knowledge of strings, combining it with what we have already learned about loops. Check out the Tutorial tab for learning materials and an instructional video.
+Today we will expand our knowledge of strings, combining it with what we have already learned about loops.
 
 Task
 Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line (see the Sample below for more detail).
