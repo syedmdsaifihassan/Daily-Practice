afff363f96c7aaae7719eb895f7c072c701bbb29
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-02-08 01:13:57 +0530

    Update Day_5.java

--- Hacker Rank (30 Days of Code)/Day_5.java
+++ Hacker Rank (30 Days of Code)/Day_5.java
@@ -1,6 +1,6 @@
 /*
 Objective
-In this challenge, we will use loops to do some math. Check out the Tutorial tab to learn more.
+In this challenge, we will use loops to do some math.
 
 Task
 Given an integer, n, print its first 10 multiples. Each multiple n*i (where 1<=i<=10 ) should be printed on a new line in the form: n x i = result.
