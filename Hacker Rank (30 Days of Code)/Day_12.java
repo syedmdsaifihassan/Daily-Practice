1839912366271c266c814f222b3a18b9f93410cb
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-02-13 20:01:41 +0530

    Update Day_12.java

--- Hacker Rank (30 Days of Code)/Day_12.java
+++ Hacker Rank (30 Days of Code)/Day_12.java
@@ -2,7 +2,7 @@
 
 /*
 Objective
-Today, we're delving into Inheritance. Check out the attached tutorial for learning materials and an instructional video.
+Today, we're delving into Inheritance.
 
 Task
 You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
