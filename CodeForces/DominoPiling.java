a503070a1ee12c4a00a6914519faecd13cb71c7e
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-04-14 00:07:10 +0530

    Create DominoPiling.java

---
+++ CodeForces/DominoPiling.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+/*
+50A	Domino piling
+
+A. Domino piling
+time limit per test: 2 seconds
+memory limit per test: 256 megabytes
+input: standard input
+output: standard output
+
+You are given a rectangular board of M × N squares. Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. You are allowed to rotate the pieces. You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
+
+1. Each domino completely covers two squares.
+
+2. No two dominoes overlap.
+
+3. Each domino lies entirely inside the board. It is allowed to touch the edges of the board.
+
+Find the maximum number of dominoes, which can be placed under these restrictions.
+
+Input
+In a single line you are given two integers M and N — board sizes in squares (1 ≤ M ≤ N ≤ 16).
+
+Output
+Output one number — the maximal number of dominoes, which can be placed.
+
+Examples
+
+input
+2 4
+output
+4
+
+input
+3 3
+output
+4
+*/
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int result;
+
+        result =(m*n)/2;                //area of each domino pieces = 2 x 1 squares
+        System.out.println(result);
+    }
+}
