3863c6bbe1b39136db36ec2ed235538442582b55
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-07-03 00:13:29 +0530

    Create Pattern19.java

---
+++ PepCoding/Pattern19.java
@@ -0,0 +1,77 @@
+/*
+Pattern 19
+
+1. You are given a number n.
+2. You've to write code to print the pattern given in output format below
+
+Input Format
+A number n
+
+Output Format
+*	*	*		*	
+		*		*	
+*	*	*	*	*	
+*		*			
+*		*	*	*	
+
+Constraints
+1 <= n <= 10
+ Also, n is odd.
+
+Sample Input
+5
+
+Sample Output
+*	*	*		*	
+		*		*	
+*	*	*	*	*	
+*		*			
+*		*	*	*	
+
+*/
+
+import java.util.*;
+
+public class Pattern19 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // write ur code here
+    int n = sc.nextInt();
+    
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            if(i==1) {                      // upper row
+                if(j<=n/2+1 || j==n) {
+                    System.out.print("*\t");
+                }else {
+                    System.out.print("\t");
+                }
+            }else if(i>1 && i<n/2+1) {      // upper middle row
+                if(j==n/2+1 || j==n) {
+                    System.out.print("*\t");
+                }else {
+                    System.out.print("\t");
+                }
+            }else if(i==n/2+1) {            // middle row
+                System.out.print("*\t");
+            }else if(i>n/2+1 && i<n) {      // last middle row
+                if(j==n/2+1 || j==1) {
+                    System.out.print("*\t");
+                }else {
+                    System.out.print("\t");
+                }
+            }else {                         // last row
+                if(j>=n/2+1 || j==1) {
+                    System.out.print("*\t");
+                }else {
+                    System.out.print("\t");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+  }
+}
