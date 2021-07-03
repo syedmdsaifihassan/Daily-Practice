5a33ad8a771d3c5d2f0569aa11f9c2a5dab2e5e7
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-07-04 01:46:54 +0530

    Update BonusPattern1.java

--- PepCoding/BonusPattern1.java
+++ PepCoding/BonusPattern1.java
@@ -7,19 +7,32 @@ public class BonusPattern1 {
     // write ur code here
     int n = sc.nextInt();
     int num1 = n/2*3;
-    
-    for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < n; j++)
-                {
-                // center horizontal, center vertical, upper left diagonal, bottom left diagonal, upper right diagonal, bottom right diagonal
-                if(i == n / 2 || j == n / 2 || i + j == n / 2 || i - j == n / 2 || j - i == n / 2 || i + j == num1)
-                    System.out.print("*\t");
-                else
-                    System.out.print("\t");
-            }
+      
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+          // center horizontal, center vertical, upper left diagonal, bottom left diagonal, upper right diagonal, bottom right diagonal
+          if(i == n / 2 || j == n / 2 || i + j == n / 2 || i - j == n / 2 || j - i == n / 2 || i + j == num1){
+              System.out.print("*\t");
+          }else{
+              System.out.print("\t");
+          }
+      }
         System.out.println();
     }
 
   }
 }
+
+/*Output
+
+                                *
+                        *       *       *
+                *               *               *
+        *                       *                       *
+*       *       *       *       *       *       *       *       *
+        *                       *                       *
+                *               *               *
+                        *       *       *
+                                *
+
+*/
