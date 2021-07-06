02c334b1998d1ec9fac4777fefc8a5188ddfca65
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-07-06 23:30:54 +0530

    Create BarChart.java

---
+++ Function and Arrays/BarChart.java
@@ -0,0 +1,73 @@
+/*
+Bar Chart
+
+1. You are given a number n, representing the size of array a.
+2. You are given n numbers, representing elements of array a.
+3. You are required to print a bar chart representing value of arr a.
+
+Input Format
+A number n
+n1
+n2
+.. n number of elements
+
+Output Format
+A bar chart of asteriks representing value of array a
+
+Constraints
+1 <= n <= 30
+0 <= n1, n2, .. n elements <= 10
+
+Sample Input
+5
+3
+1
+0
+7
+5
+
+Sample Output
+			*		
+			*		
+			*	*	
+			*	*	
+*			*	*	
+*			*	*	
+*	*		*	*
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BarChart {
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    
+    for(int i=0; i<n; i++){
+        arr[i]=sc.nextInt();
+    }
+    int m = arr[0];
+    for(int i=0; i<n; i++){
+        if(m<arr[i]){
+            m=arr[i];
+        }
+    }
+    
+    for(int i=m; i>=1; i--){
+        for(int j=0; j<n; j++){
+            if(i>arr[j]){
+                System.out.print("\t");
+            }else{
+                System.out.print("*\t");
+            }
+        }
+        System.out.println();
+    }
+ }
+
+}
