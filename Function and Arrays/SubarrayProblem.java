b3039e8ffb6f424bf74887c3307775f7d2c80cd0
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-07-07 18:47:18 +0530

    Update SubarrayProblem.java

--- Function and Arrays/SubarrayProblem.java
+++ Function and Arrays/SubarrayProblem.java
@@ -53,13 +53,11 @@ public static void main(String[] args) throws Exception {
     }
     
     for(int i=0; i<n; i++){
-        for(int j=i; j<=n; j++){
-            for(int k=i; k<j; k++){
+        for(int j=i; j<n; j++){
+            for(int k=i; k<=j; k++){
                 System.out.print(arr[k]+"\t");
-                if(k==j-1){
-                    System.out.println();
-                }
             }
+            System.out.println();
         }
     }
     
