0140bf496707524f304be1976e3fd6ca0681d0d2
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-07-22 14:53:55 +0530

    Update Powerlinear.java

--- Introduction to Recursion/Powerlinear.java
+++ Introduction to Recursion/Powerlinear.java
@@ -31,7 +31,7 @@ Sample Output
 import java.io.*;
 import java.util.*;
 
-public class PowerLinear {
+public class Powerlinear {
 
     public static void main(String[] args) throws Exception {
         // write your code here
