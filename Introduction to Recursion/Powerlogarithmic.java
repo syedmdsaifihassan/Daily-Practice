d38c59a075ff5918134c75820bd8ea722295fef7
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-07-22 14:55:56 +0530

    Create Powerlogarithmic.java

---
+++ Introduction to Recursion/Powerlogarithmic.java
@@ -0,0 +1,61 @@
+/*
+Power-logarithmic
+Easy
+
+1. You are given a number x.
+2. You are given another number n.
+3. You are required to calculate x raised to the power n. Don't change the signature of power function.
+
+Note1 -> The previous version expects the call stack to be of n height. This function expects call function to be only log(n) high.
+
+Note2 -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+Input Format
+A number x
+A number n
+
+Output Format
+x raised to the power n
+
+Constraints
+1 <= x <= 10
+0 <= n <= 9
+
+Sample Input
+2
+5
+
+Sample Output
+32
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Powerlogarithmic {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        
+        System.out.println(power(x,n));
+    }
+
+    public static int power(int x, int n){
+        if(n==0){
+            return 1;
+        }
+        
+        int a = power(x, n/2);
+        
+        if(n%2==0){
+            return a*a;
+        }else{
+            return a*a*x;
+        }
+    }
+
+}
