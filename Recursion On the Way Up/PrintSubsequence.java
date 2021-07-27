c218a86e26e4b3027edee13ce1499fba62317d68
SYED MD SAIFI HASSAN <syedsaifihassan@gmail.com>
2021-07-27 20:19:42 +0530

    Create PrintSubsequence.java

---
+++ Recursion On the Way Up/PrintSubsequence.java
@@ -0,0 +1,63 @@
+/*
+Print Subsequence
+Easy
+
+1. You are given a string str.
+2. Complete the body of printSS function - without changing signature - to calculate and print all subsequences of str.
+Use sample input and output to take idea about subsequences.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+
+Input Format
+A string str
+
+Output Format
+Subsequences of str in order hinted by Sample output
+
+Constraints
+0 <= str.length <= 7
+
+Sample Input
+yvTA
+
+Sample Output
+yvTA
+yvT
+yvA
+yv
+yTA
+yT
+yA
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class PrintSubsequence {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String ans = "";
+        printSS(str, ans);
+    }
+
+    public static void printSS(String str, String ans) {
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+        
+        
+        // yes
+        printSS(ros, ans+ch);
+        // no
+        printSS(ros, ans);
+    }
+    
+
+}
