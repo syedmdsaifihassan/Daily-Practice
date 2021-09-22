f0e750b3686792a1a698498ee9b3cc98ed7f3da8
SYED MD SAIFI HASSAN <syedsaifihassan@gmail.com>
2021-09-22 12:20:50 +0530

    Create 4.3.20 Kth Largest Element In Tree.java

---
+++ 4.3 Generic Tree/4.3.20 Kth Largest Element In Tree.java
@@ -0,0 +1,121 @@
+/*
+Kth Largest Element In Tree
+Medium
+
+1. You are given a partially written GenericTree class.
+2. You are given a number k. You are required to find and print the kth largest value in the tree.
+3. Input and Output is managed for you.
+
+Sample Input
+24
+10 20 -50 -1 60 -1 -1 30 70 -1 -80 110 -1 -120 -1 -1 90 -1 -1 40 -100 -1 -1 -1
+8
+
+Sample Output
+10
+
+
+Asked in Companies
+Amazon      8
+Oracle      2
+Facebook        5
+TripleByte      2
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  
+  static int ceil;
+  static int floor;
+  public static void ceilAndFloor(Node node, int data) {
+    if(node.data > data){
+      if(node.data < ceil){
+        ceil = node.data;
+      }
+    }
+
+    if(node.data < data){
+      if(node.data > floor){
+        floor = node.data;
+      }
+    }
+
+    for (Node child : node.children) {
+      ceilAndFloor(child, data);
+    }
+  }
+
+  public static int kthLargest(Node node, int k){
+    // write your code here
+    int data = Integer.MAX_VALUE;
+    floor = Integer.MIN_VALUE;
+    for(int i=0; i<k; i++){
+        ceilAndFloor(node, data);
+        data = floor;
+        floor = Integer.MIN_VALUE;
+    }
+    return data;
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    int k = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    int kthLargest = kthLargest(root, k);
+    System.out.println(kthLargest);
+  }
+
+}
