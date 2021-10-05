5a8c2b2dd7b8f477b28bae197dc15dddb12e0fcf
SYED MD SAIFI HASSAN <syedsaifihassan@gmail.com>
2021-10-05 11:51:44 +0530

    ArrayList Demo

---
+++ Others/TestArrayList.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+public class TestArrayList {
+    public static void main(String args[]) {
+        // Scanner sc = new Scanner(System.in);
+        
+        // ArrayList<Integer> al = new ArrayList<>();
+        // // for(int i=0; i<10; i++){
+        // //     int n = sc.nextInt();
+        // //     al.add(n);
+        // // }
+        // al.add(2);
+        // al.add(4);
+        // al.add(4);
+        // al.add(4);
+        // al.add(4);
+        // al.add(4);
+        // al.add(34);
+        // al.add(47);
+        // al.add(40);
+        // al.add(43);
+        // al.add(4);
+        // al.add(4);
+        // al.add(4);
+        // al.add(4);
+        
+        // // System.out.println(al.size());
+        
+        // for(int i=0; i<al.size(); i++){
+        //     System.out.print(al.get(i)+" ");
+        // }
+        
+        ArrayList<String> al = new ArrayList<>();
+        al.add("this");
+        al.add("is");
+        al.add("array list");
+        al.add("of");
+        al.add("string");
+        
+        System.out.println(al.size());
+        // for(int i=0; i<al.size(); i++){
+        //     System.out.println(al.get(i));
+        // }
+        
+    }
+}
