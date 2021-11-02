694557742f3088bff5a6d82d10fbff7facc2ccf1
SYED MD SAIFI HASSAN <syedsaifihassan@gmail.com>
2021-11-02 20:29:42 +0530

    Create MedianofTwoSortedArrays.java

---
+++ Problem Solving/MedianofTwoSortedArrays.java
@@ -0,0 +1,76 @@
+/*
+4. Median of Two Sorted Arrays
+Hard
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+
+Example 2:
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+Example 3:
+Input: nums1 = [0,0], nums2 = [0,0]
+Output: 0.00000
+
+Example 4:
+Input: nums1 = [], nums2 = [1]
+Output: 1.00000
+
+Example 5:
+Input: nums1 = [2], nums2 = []
+Output: 2.00000
+
+Constraints:
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-10^6 <= nums1[i], nums2[i] <= 10^6
+
+*/
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] mergeArr = new int[n+m];
+        int i=0, j=0, k=0;
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                mergeArr[k] = nums1[i];
+                i++;
+            }else{
+                mergeArr[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n){
+            mergeArr[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while(j<m){
+            mergeArr[k] = nums2[j];
+            j++;
+            k++;
+        }
+        
+        double med = 0;
+        if(mergeArr.length%2==0){
+            med = (double)mergeArr[(mergeArr.length/2)-1] + (double)mergeArr[(mergeArr.length/2)];
+            return med/2;
+        }else{
+            return (double)mergeArr[mergeArr.length/2];
+        }
+    }
+}
