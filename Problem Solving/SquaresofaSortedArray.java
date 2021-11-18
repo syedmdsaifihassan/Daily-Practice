da946e866e97920709a9c7b53bbffc2b7a190749
SYED MD SAIFI HASSAN <syedsaifihassan@gmail.com>
2021-11-18 17:08:01 +0530

    Update SquaresofaSortedArray.java

--- Problem Solving/SquaresofaSortedArray.java
+++ Problem Solving/SquaresofaSortedArray.java
@@ -39,3 +39,22 @@ class Solution {
         return nums;
     }
 }
+
+// O(n)
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] res = new int[nums.length];
+        int left = 0, right = nums.length-1;
+        for(int i=res.length-1; i>=0; i--){
+            if(Math.abs(nums[left])>Math.abs(nums[right])){
+                res[i] = nums[left]*nums[left];
+                left++;
+            }else{
+                res[i] = nums[right]*nums[right];
+                right--;
+            }
+        }
+        
+        return res;
+    }
+}
