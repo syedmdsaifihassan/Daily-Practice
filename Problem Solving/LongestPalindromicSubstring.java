380b1ef2c59499f2dcb964122bbbc931cf7bc274
SYED MD SAIFI HASSAN <syedsaifihassan@gmail.com>
2021-12-29 02:55:17 +0530

    Update LongestPalindromicSubstring.java

--- Problem Solving/LongestPalindromicSubstring.java
+++ Problem Solving/LongestPalindromicSubstring.java
@@ -19,6 +19,7 @@ s consist of only digits and English letters.
 
 */
 
+/* Solution 1 */
 class Solution {
     public String longestPalindrome(String s) {
         int ans = Integer.MIN_VALUE;
@@ -36,6 +37,38 @@ class Solution {
             }
             p1=i-1;
             p2=i;
+            while((p1>=0 && p2<s.length()) && s.charAt(p1) == s.charAt(p2)){
+                if(ans < (p2-p1)+1){
+                    ans = (p2-p1)+1;
+                    res = s.substring(p1, p2+1);
+                }
+                p1--;
+                p2++;
+            }
+        }
+        if(res.length()==0) return s.charAt(0)+"";
+        return res;
+    }
+}
+
+/* Solution 2 */
+class Solution {
+    public String longestPalindrome(String s) {
+        int ans = Integer.MIN_VALUE;
+        String res = "";
+        for(int i=0; i<s.length()-1; i++){
+            int p1 = i-1;
+            int p2 = i+1;
+            while((p1>=0 && p2<s.length()) && s.charAt(p1) == s.charAt(p2)){
+                if(ans < (p2-p1)+1){
+                    ans = (p2-p1)+1;
+                    res = s.substring(p1, p2+1);
+                }
+                p1--;
+                p2++;
+            }
+            p1=i;
+            p2=i+1;
             while((p1>=0 && p2<s.length()) && s.charAt(p1) == s.charAt(p2)){
                 if(ans < (p2-p1)+1){
                     ans = (p2-p1)+1;
